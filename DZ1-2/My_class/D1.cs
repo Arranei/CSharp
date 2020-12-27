c1a4345ad8cde1e57bbda32fb792fb4e3f942003
Arranei <76662963+Arranei@users.noreply.github.com>
2020-12-27 22:28:07 +0300

    Add files via upload

---
+++ DZ1-2/My_class/D1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace My_class
+{
+    [Serializable]
+    public class MeteoSystem
+    {
+        public string SName { get; set; }
+        public int SNumber { get; set; }
+
+        public Line Line { get; set; }
+        public MeteoSystem()
+        { }
+
+        public MeteoSystem(string name, int num, Line line)
+        {
+            SName = name;
+            SNumber = num;
+            Line = line;
+        }
+
+       
+    }
+
+    [Serializable]
+    public class Line
+    {
+        public int LNumber { get; set; }
+
+        public Sensor Sensor { get; set; }
+        public Line()
+        { }
+
+        public Line(int num, Sensor sensor)
+        {
+            LNumber = num;
+            Sensor = sensor;
+        }
+        
+    }
+
+    [Serializable]
+    public class Sensor
+    {
+        public int Pressure { get; set; }
+        public int Viscosity { get; set; }
+        public int Density { get; set; }
+        public int T { get; set; }
+        public int QuantityMax { get; set; }
+        public int QuantityMin { get; set; }
+        public string ShortName { get; set; }
+
+        public Sensor()
+        { }
+
+        public Sensor(int pre, int vis, int den, int t, int qma, int qmi, string name)
+        {
+            Pressure = pre;
+            Viscosity = vis;
+            Density = den;
+            T = t;
+            QuantityMax = qma;
+            QuantityMin = qmi;
+            ShortName = name;
+        }
+
+    }
+}
---
+++ DZ1-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+using My_class;
+// VAR DX1
+namespace DZ1_2
+{
+    class Program
+    {
+#pragma warning disable IDE0060 // Удалите неиспользуемый параметр
+        static void Main(string[] args)
+#pragma warning restore IDE0060 // Удалите неиспользуемый параметр
+        {
+            Sensor TestSensor = new Sensor(1, 2, 3, 4, 5, 6, "Проба");
+            Line TestLine = new Line(0, TestSensor);
+            MeteoSystem Test = new MeteoSystem("Тестовая линия", 42, TestLine);
+
+            XmlSerializer formatter = new XmlSerializer(typeof(MeteoSystem));
+
+            using (FileStream fs = new FileStream("MeteoTest.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, Test);
+            }
+            Console.WriteLine("DZ1-2 Var ");
+        }
+    }
+}
